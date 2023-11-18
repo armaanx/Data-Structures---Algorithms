7c19bbbba6edd1885e6c5417158e0d1601bc42ce
Armaan Mishra <armaanmishra48@gmail.com>
2023-11-19 04:02:35 +0530

    Refactor array insertion and add new methods

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -3,17 +3,20 @@ package Array;
 public class Main {
     public static void main(String args[]) {
         myArray arr = new myArray(10);
-        arr.insert(1);
-        arr.insert(2);
-        arr.insert(3);
+        arr.insert(11);
+        arr.insert(12);
+        arr.insert(31);
         arr.insert(14);
-        arr.insert(9);
+        arr.insert(91);
         
         //arr.removeAt(0);
         //System.out.println(arr); 
+        //arr.reverse();
+        //System.out.println(arr); 
         //System.out.println(arr.indexOf(1));
         //System.out.println(arr.max());
-        
+        //System.out.println(arr.min());
+
 
     }
 }
--- src/Array/myArray.java
+++ src/Array/myArray.java
@@ -9,6 +9,13 @@ public class myArray {
     }
 
     public void insert(int value) {
+        if (currentIndex == items.length) {
+            int[] temp = new int[items.length * 2];
+            for (int i = 0; i < currentIndex; i++) {
+                temp[i] = items[i];
+            }
+            items = temp;
+        }
         this.items[currentIndex] = value;
         currentIndex++;
     }
@@ -50,5 +57,27 @@ public class myArray {
             }
         }
         return result;
+    }
+
+    public int min() {
+        int result = this.items[0];
+        for (int i = 1; i < this.currentIndex; i++) {
+            if (this.items[i] < result) {
+                result = this.items[i];
+            }
+        }
+        return result;
+    }
+    
+    public void reverse() {
+        int i = 0;
+        int j = this.currentIndex - 1;
+        while(i < j) {
+            int temp = items[i];
+            items[i] = items[j];
+            items[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
