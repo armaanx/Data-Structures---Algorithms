0ca79b3aca7ab7c63d4dbc3bc0da9e7dac9532d0
Armaan Mishra <armaanmishra48@gmail.com>
2023-11-19 03:08:54 +0530

    initial commit

---
+++ src/Array/Main.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Main {
+    public static void main(String args[]) {
+        myArray arr = new myArray(10);
+        arr.insert(1);
+        arr.insert(2);
+        arr.insert(3);
+        arr.insert(4);
+        arr.insert(9);
+        
+        //arr.removeAt(0);
+        //System.out.println(arr); 
+        //System.out.println(arr.indexOf(1));
+        
+
+    }
+}
---
+++ src/Array/myArray.java
@@ -0,0 +1,45 @@
+package Array;
+public class myArray {
+    private int[] items;
+    private int currentIndex;
+
+    public myArray(int initialSize) {
+        this.items = new int[initialSize];
+        this.currentIndex = 0;
+    }
+
+    public void insert(int value) {
+        this.items[currentIndex] = value;
+        currentIndex++;
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        for (int i = 0; i < this.currentIndex; i++) {
+            str.append(items[i] + ", ");
+        }
+        str.append('\b').append('\b').append("]");
+        return str.toString();
+    }
+    
+    public int indexOf(int value) {
+        for (int i = 0; i <= this.currentIndex; i++) {
+            if (this.items[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void removeAt(int index) {
+        if (index >= currentIndex)
+            throw new IllegalArgumentException();
+        for (int i = index; i <= this.currentIndex - 2; i++) {
+            this.items[i] = this.items[i + 1];
+        }
+        currentIndex--;
+        this.items[currentIndex] = 0;
+
+    }
+}
